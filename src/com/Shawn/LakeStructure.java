9dd18f5b122d0761c03638204e8a0f33847f021c
Wolfknightx <rc5340oc@go.minneapolis.edu>
2016-02-29 19:03:43 -0600

    Created a structure to handle getting the information stored into variable for creating lakes and finding the best time.

---
+++ src/com/Shawn/LakeStructure.java
@@ -0,0 +1,28 @@
+package com.Shawn;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Wolfknightx on 2/29/2016.
+ * This will handle what information is tied to a lake when building a lake
+ */
+public class LakeStructure {
+    String lakeName = "";
+    ArrayList<Double> lakeTimes = new ArrayList<Double>();
+ //   double lakeTime = 0;
+   /* public String lakeConstructor(){
+        //when this is called it should store the information for the lakes in this format.
+        return lakeName + ", " + lakeTime + ".";
+    }*/
+
+    public String bestLakeTime(){
+        double timer = 999999999;
+        //pass back the best time around the lake(s).
+        for (int x = 0; x < lakeTimes.size(); x++){
+            if (lakeTimes.get(x) < timer){
+                timer = lakeTimes.get(x);
+            }
+        }
+        return lakeName + "," + timer + ".";
+    }
+}
--- src/com/Shawn/Main.java
+++ src/com/Shawn/Main.java
@@ -1,8 +1,48 @@
 package com.Shawn;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Create a timer which tracks the time around the lake.  Later we will grab the fastest times for each lake.
+
+        //Variables and storage information
+        ArrayList<LakeStructure> lakes = new ArrayList<LakeStructure>(); //used the structure as a means of control.
+        Scanner scanner = new Scanner(System.in);
+        String lakeName = "";
+        double lakeTime = 0;
+        LakeStructure myLakeInfo = new LakeStructure();
+
+        //display welcome and grab the name of the first lake
+        System.out.println("Welcome to your lake timer, please enter the name of your first lake.");
+        String newLakeName = scanner.nextLine();
+        lakeName = newLakeName; //push the info into the name.
+
+        //now get the time for the lake.
+        System.out.println("Thanks! Now what was your time around the lake?");
+        double newLakeTime = scanner.nextDouble();
+        lakeTime = newLakeTime; //push the info into the time.
+
+        //use the information and fill in the structure to build that needed data then add it to the arraylist.
+        myLakeInfo.lakeName = lakeName;
+        myLakeInfo.lakeTime = lakeTime;
+
+        if (lakeExistYN(lakes,lakeName) == false){
+            lakes.add(myLakeInfo); //if the new
+        }
+
+
+    }
+
+    public static boolean lakeExistYN(ArrayList<LakeStructure> lakes, String name){
+        //this will be used to handle checking if the lake already exist or not.
+        for (LakeStructure lake: lakes) {
+            if (lake.lakeName == name){
+                return true;
+            }
+        }
+        return false;
     }
 }
